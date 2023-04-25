b09a8d2770de133cf6a01aaa2a2ba66aa43a29e4
Minilik94 <minilikzeru@gmail.com>
2023-04-25 04:21:54 +0300

    updated

--- models/userModel.js
+++ models/userModel.js
@@ -3,5 +3,20 @@ const validator = require('validator')
 const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: [true, "Please tell us your name"]
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: [true, "Please provide your email address"], 
+        validate: [validator.isEmail, "Please provide your email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 8
+    }
 })
