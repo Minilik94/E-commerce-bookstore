0c9c1507b9d7b78f4d3b92f766bb773bab1d4a6c
Minilik94 <minilikzeru@gmail.com>
2023-04-25 04:30:30 +0300

    update

--- models/userModel.js
+++ models/userModel.js
@@ -14,9 +14,18 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please provide your email address"], 
         validate: [validator.isEmail, "Please provide your email address"]
     },
+    photo: String,
     password: {
         type: String,
         required: [true, "Please provide a password"],
         minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please confirm your password"]
     }
 })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
