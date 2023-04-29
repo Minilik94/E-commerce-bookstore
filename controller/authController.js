f6052b66d535002f7b59820333079810592bd0bf
Minilik94 <minilikzeru@gmail.com>
2023-04-29 13:44:43 +0300

    Protected Route

--- controller/authController.js
+++ controller/authController.js
@@ -1,87 +1,132 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
 
 const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
 
 exports.signUp = async (req, res) => {
-  try {
-    const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-    });
+    try {
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm
+        })
 
-    const token = signToken(newUser._id);
+        const token = signToken(newUser._id)
 
-    res.status(201).json({
-      status: 'success',
-      token,
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+        res.status(201).json({
+            status: 'success',
+            token,
+            data: {
+                user: newUser
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error.message
+        })
+    }
+}
 
 exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body
 
-    if (!email || !password) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Please provide email and password',
-      });
+        if (!email || !password) {
+            return res.status(400).json({
+                status: 'fail',
+                message: 'Please provide email and password'
+            })
+        }
+
+        const user = await User.findOne({ email }).select('+password')
+
+        if (!user || !(await user.correctPassword(password, user.password))) {
+            return res.status(401).json({
+                status: false,
+                message: 'Incorrect email or password'
+            })
+        }
+
+        const token = signToken(user._id)
+        res.status(200).json({
+            status: true,
+            token
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(404).json({
+            status: 'fail',
+            message: error.message
+        })
     }
-
-    const user = await User.findOne({ email }).select('+password');
-
-    if (!user || !(await user.correctPassword(password, user.password))) {
-      return res.status(401).json({
-        status: false,
-        message: 'Incorrect email or password',
-      });
-    }
-
-    const token = signToken(user._id);
-    res.status(200).json({
-      status: true,
-      token,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(404).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+}
 
 // Protecting the book route
 
 exports.protect = async (req, res, next) => {
-  try {
-// 1) Check if the token exists 
-// 2) Verification of token
-// 3) Check if the user still exists
-// 4) Check if the user changed the password after the token was issued
-    next();
-  } catch (error) {
-    console.log(error.message);
-    res.status(404).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-  next();
-};
+    try {
+        // 1) Check if the token exists
+        let token
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith('Bearer')
+        ) {
+            token = req.headers.authorization.split(' ')[1]
+        }
+        if (!token) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'You are not logged in! Please login'
+            })
+        }
+        // 2) Verification of token
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded)
+
+        // 3) Check if the user still exists
+        const currentUser = await User.findById(decoded.id)
+        if (!currentUser) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'The user belonging to this token no longer exist'
+            })
+        }
+        // 4) Check if the user changed the password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'User recently changed password. Please login again!'
+            })
+        }
+
+        // Grant access to protected routes
+        req.user = currentUser
+        next()
+    } catch (error) {
+        console.log(error.message)
+        if(process.env.NODE_ENV === 'production'){
+          if(error.name === 'JsonWebTokenError'){
+            return res.status(401).json({
+              status: 'fail',
+              message: 'Invalid token Please login again!'
+            })
+          }
+          if(error.name === 'TokenExpiredError'){
+            return res.status(401).json({
+              status: 'fail',
+              message: 'Your token has expired please login again!'
+            })
+          }
+        }
+        res.status(404).json({
+            status: 'fail',
+            message: error.message
+        })
+    }
+}
--- models/userModel.js
+++ models/userModel.js
@@ -1,55 +1,66 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please tell us your name'],
-  },
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    required: [true, 'Please provide your email address'],
-    validate: [validator.isEmail, 'Please provide your email address'],
-  },
-  photo: String,
-  password: {
-    type: String,
-    required: [true, 'Please provide a password'],
-    minlength: 8,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    validate: {
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: 'Passwords are not the same',
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name']
     },
-    select: false,
-  },
-});
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: [true, 'Please provide your email address'],
+        validate: [validator.isEmail, 'Please provide your email address']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function (el) {
+                return el === this.password
+            },
+            message: 'Passwords are not the same'
+        },
+        select: false
+    },
+    passwordChangedAt: Date
+})
 
 userSchema.pre('save', async function (next) {
-  // Run only If password is modefied
-  if (!this.isModified('password')) return next();
+    // Run only If password is modefied
+    if (!this.isModified('password')) return next()
 
-  this.password = await bcrypt.hash(this.password, 12);
-  //   Delete the password confirm field
-  this.passwordConfirm = undefined;
-  next();
-});
+    this.password = await bcrypt.hash(this.password, 12)
+    //   Delete the password confirm field
+    this.passwordConfirm = undefined
+    next()
+})
 
 userSchema.methods.correctPassword = async function (
-  candidatePassword,
-  userPassword
+    candidatePassword,
+    userPassword
 ) {
-  return await bcrypt.compare(candidatePassword, userPassword);
-};
+    return await bcrypt.compare(candidatePassword, userPassword)
+}
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  let changedTimestamp
+    if (this.passwordChangedAt) {
+         changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10)
+        console.log(changedTimestamp)
+    }
 
-module.exports = User;
+    return JWTTimestamp < changedTimestamp
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
