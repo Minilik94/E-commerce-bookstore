24ca97fab4914d7891fde403e8d6c44d22920e12
Minilik94 <minilikzeru@gmail.com>
2024-11-10 23:55:15 +0300

    aded

--- app.js
+++ app.js
@@ -13,16 +13,29 @@ const cors = require('cors')
 
 const app = express()
 
-// Set security HTTP
-app.use(helmet())
+// Set security HTTP headers with Helmet and configure CSP
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'"],
+                connectSrc: ["'self'", "https://*.stripe.com"],
+                imgSrc: ["'self'", "data:", "https://*.stripe.com"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                formAction: ["'self'", "https://*.stripe.com"],
+                frameAncestors: ["'self'"]
+            },
+        },
+    })
+);
 
+// Configure CORS
 const corsOptions = {
     origin: '*',
-    methods: 'GET,POST,PATCH,DELETE' 
-  }
-  
-
-app.use(cors(corsOptions)) // Use cors with custom options
+    methods: 'GET,POST,PATCH,DELETE'
+}
+app.use(cors(corsOptions))
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
@@ -31,17 +44,15 @@ if (process.env.NODE_ENV === 'development') {
 // Rate limiter
 const limiter = rateLimit({
     max: 10,
-    windowMs: 15 * 60 * 100
+    windowMs: 15 * 60 * 1000
 })
+app.use('/api', limiter)
 
-// app.use('/api', limiter);
-
-// Body parser, reading data from req.body
+// Body parser
 app.use(express.json({ limit: '10kb' }))
 
-// Data sanitization against Nosql query injection
+// Data sanitization
 app.use(mongoSanitize())
-// Data sanitization against XSS
 app.use(xss())
 
 // Preventing parameter pollution
@@ -51,33 +62,18 @@ app.use(
     })
 )
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome To Books API')
-// })
-
-// app.options('/api/books', (req, res) => {
-//     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1')
-//     res.header('Access-Control-Allow-Methods','POST, PATCH, DELETE')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//     res.sendStatus(204) // No content
-// })
-// app.options('/api/users', (req, res) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:4173', 'http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:3000')
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//     res.sendStatus(204) // No content
-// })
-
+// Routes
 app.use('/api/books', bookRouter)
 app.use('/api/users', userRouter)
 app.use('/api/reviews', reviewRouter)
 app.use('/api/ordering', orderRouter)
 
 // Serving static files
-app.use(express.static(`${__dirname}/client/build`));
+app.use(express.static(`${__dirname}/client/build`))
 
+// Import and use the handler for the client build
 import("././client/build/handler.js").then(({ handler }) => {
-  app.use(handler);
+  app.use(handler)
 });
 
 module.exports = app
